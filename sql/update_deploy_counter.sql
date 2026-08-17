2478fe0eed559dd50e1162a169b5c44b4bcca1af
Javed-HomeLaptop <ji2481632@yahoo.com>
2026-08-17 18:08:12 -0400

    Revert back previous changes
    Removed insert statement and simplified the query to select the next deploy counter.

--- sql/update_deploy_counter.sql
+++ sql/update_deploy_counter.sql
@@ -1,8 +1,3 @@
 insert into `__GCP_PROJECT_ID__.__SCHEMA__.tracker`
-  (ins_count, ins_ts, env_name, project_id)
-select  max(ins_count) +1
-      , current_timestamp()
-      , '__ENVNAME__'
-      , '__GCP_PROJECT_ID__'
+select max(ins_count) +1, current_timestamp()
 from   `__GCP_PROJECT_ID__.__SCHEMA__.tracker`;
-
